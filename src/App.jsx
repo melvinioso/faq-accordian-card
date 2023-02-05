2e7b2a2d5c60449f608b583ae98c1d02179255d8
Matt McGavern <matt.mcgavern@gmail.com>
2023-02-05 14:54:00 -0600

    FAQ component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
+import FAQ from './components/FAQ';
 
 function App() {
   return (
     <div className="App">
-
+      <FAQ />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/FAQ/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './FAQ.module.css';
+
+const FAQ = (props) => {
+  return <div>FAQ Card</div>;
+};
+
+export default FAQ;

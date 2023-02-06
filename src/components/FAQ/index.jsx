9d0df3eed3ab91395c1162cd5987789fe5a4db1c
Matt McGavern <matt.mcgavern@gmail.com>
2023-02-05 18:38:58 -0600

    background images working

--- src/components/FAQ/index.jsx
+++ src/components/FAQ/index.jsx
@@ -4,9 +4,28 @@ import styles from './FAQ.module.css';
 const FAQ = (props) => {
   return (
     <div className={styles.container}>
-      FAQ Card
+      <div className={styles.images}>
+        <img
+          className={styles.box}
+          src="/illustration-box-desktop.svg"
+          alt="box"
+        />
+        <img
+          className={styles.pattern}
+          src="/bg-pattern-desktop.svg"
+          alt="pattern"
+        />
+        <img
+          className={styles.illustration}
+          src="/illustration-woman-online-desktop.svg"
+          alt="illustration"
+        />
+      </div>
+      <div className={styles.faq}>
+        <h1 className={styles.title}>FAQ</h1>
+      </div>
     </div>
-  )
+  );
 };
 
 export default FAQ;

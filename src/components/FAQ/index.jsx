746d3dbdcaa5477c0c89310d2e37add75b52a82b
Matt McGavern <matt.mcgavern@gmail.com>
2023-02-06 14:15:29 -0600

    working accordian

--- src/components/FAQ/index.jsx
+++ src/components/FAQ/index.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cx from 'classnames';
 import styles from './FAQ.module.css';
 
+const questions = [
+  {
+    id: 1,
+    question: 'How many team members can I invite?',
+    answer:
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+  },
+  {
+    id: 2,
+    question: 'What is the maximum file upload size?',
+    answer:
+      'No more than 2GB. All files in your account must fit your allotted storage space.',
+  },
+  {
+    id: 3,
+    question: 'How do I reset my password?',
+    answer:
+      'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
+  },
+  {
+    id: 4,
+    question: 'Can I cancel my subscription?',
+    answer:
+      'Yes! Send us a message and we’ll process your request no questions asked.',
+  },
+  {
+    id: 5,
+    question: 'Do you provide additional support?',
+    answer:
+      'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+  },
+];
+
+const Question = ({ id, question, answer, toggle, open }) => {
+  return (
+    <div key={id} className={styles.question}>
+      <div className={styles.questionRow} onClick={() => toggle(id)}>
+        <h2
+          className={cx(`${styles.questionTitle}`, {
+            [styles.active]: open,
+          })}
+        >
+          {question}
+        </h2>
+        <img
+          className={cx(`${styles.questionIcon}`, {
+            [styles.questionIconOpen]: open,
+          })}
+          src="/icon-arrow-down.svg"
+          alt="arrow"
+        />
+      </div>
+      {open && <p className={styles.questionAnswer}>{answer}</p>}
+    </div>
+  );
+};
+
 const FAQ = (props) => {
+  const [activeId, setActiveId] = useState(null);
+
+  const handleToggle = (id) => {
+    setActiveId(activeId !== id ? id : null);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.boxContainer}>
@@ -25,6 +89,16 @@ const FAQ = (props) => {
       </div>
       <div className={styles.faq}>
         <h1 className={styles.title}>FAQ</h1>
+        {questions.map(({ id, question, answer }) => (
+          <Question
+            key={id}
+            id={id}
+            question={question}
+            answer={answer}
+            toggle={handleToggle}
+            open={activeId === id}
+          />
+        ))}
       </div>
     </div>
   );
